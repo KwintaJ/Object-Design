33c513ee5341f9d18cada9ddb11ecaed09b93d56
Jan Kwinta <jankwinta@icloud.com>
2026-03-12 15:22:34 +0100

    D.1.3.0.12.03 - poprawka w zakresie

--- Zadanie-1/3_0/simpleGenerator.pas
+++ Zadanie-1/3_0/simpleGenerator.pas
@@ -2,11 +2,11 @@ program SimpleGenerator;
 var
     i: integer;
 begin
-    //--- generate and output 50 random integers ---
+    //--- generate and output 50 random integers in range [0, 100] ---
     randomize();
     for i := 0 to 49 do
     begin
-        write(random(100));
+        write(random(101));
         write(' ');
     end;
     writeln('');
