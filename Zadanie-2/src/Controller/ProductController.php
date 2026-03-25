77f9abcbdf3ff26da45bf1c168e32010a388dab0
Jan Kwinta <jankwinta@icloud.com>
2026-03-25 19:27:15 +0100

    B.2.3.0.25.03 - Product: entity + controller

---
+++ Zadanie-2/src/Controller/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\{JsonResponse, Request};
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/products')]
+class ProductController extends AbstractController
+{
+    #[Route('', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): JsonResponse {
+        $products = $em->getRepository(Product::class)->findAll();
+        return $this->json(array_map(fn($p) => ['id' => $p->getId(), 'name' => $p->getName(), 'price' => $p->getPrice()], $products));
+    }
+
+    #[Route('', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+        $product = (new Product())->setName($data['name'])->setPrice($data['price']);
+        $em->persist($product);
+        $em->flush();
+        return $this->json(['id' => $product->getId()], 201);
+    }
+
+    #[Route('/{id}', methods: ['PUT'])]
+    public function update(int $id, Request $request, EntityManagerInterface $em): JsonResponse {
+        $product = $em->getRepository(Product::class)->find($id);
+        if (!$product) return $this->json(['error' => 'Not found'], 404);
+        $data = json_decode($request->getContent(), true);
+        $product->setName($data['name'] ?? $product->getName())->setPrice($data['price'] ?? $product->getPrice());
+        $em->flush();
+        return $this->json(['message' => 'Updated']);
+    }
+
+    #[Route('/{id}', methods: ['DELETE'])]
+    public function delete(int $id, EntityManagerInterface $em): JsonResponse {
+        $product = $em->getRepository(Product::class)->find($id);
+        if ($product) { $em->remove($product); $em->flush(); }
+        return $this->json(['message' => 'Deleted']);
+    }
+}
