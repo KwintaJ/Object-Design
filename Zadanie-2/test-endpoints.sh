e22ff4418a60038a243bf4b1c3f03e9f52ec4b95
Jan Kwinta <jankwinta@icloud.com>
2026-03-25 19:43:07 +0100

    D.2.3.5.25.03 - skrypt do testowania endpointow CRUD

---
+++ Zadanie-2/test-endpoints.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+URL="http://localhost:8000/api/products"
+
+echo "--- 1. Dodanie produktu ---"
+
+RESPONSE=$(curl -s -X POST $URL \
+     -H "Content-Type: application/json" \
+     -d '{"name": "Produkt A", "price": 49.99}')
+
+echo "$RESPONSE"
+
+PRODUCT_ID=$(echo $RESPONSE | grep -oE '"id":[0-9]+' | grep -oE '[0-9]+')
+
+if [ -z "$PRODUCT_ID" ]; then
+    exit 1
+fi
+
+echo -e "Testowany produkt id=$PRODUCT_ID\n"
+
+echo "--- 2. Lista wszystkich ---"
+curl -X GET $URL
+echo -e "\n"
+
+echo "--- 3. Produkt ---"
+curl -X GET "$URL/$PRODUCT_ID"
+echo -e "\n"
+
+echo "--- 4. Aktualizacja produktu ---"
+curl -X PUT "$URL/$PRODUCT_ID" \
+     -H "Content-Type: application/json" \
+     -d '{"name": "Produkt B", "price": 50.00}'
+echo -e "\n"
+
+echo "--- 5. Produkt ---"
+curl -X GET "$URL/$PRODUCT_ID"
+echo -e "\n"
+
+echo "--- 6. Usuniecie produktu ---"
+curl -X DELETE "$URL/$PRODUCT_ID"
+echo -e "\n"
+
+echo "--- 6. Sprawdzenie listy po usunieciu ---"
+curl -X GET $URL
+echo -e "\n"
