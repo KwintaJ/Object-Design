e33ced2f66953c9f6504793f6cbe058590292400
Jan Kwinta <jankwinta@icloud.com>
2026-04-08 16:32:09 +0200

    E.3.4.5.08.04 - autowired constructor injection

--- Zadanie-3/app/src/main/kotlin/com/example/app/AppApplication.kt
+++ Zadanie-3/app/src/main/kotlin/com/example/app/AppApplication.kt
@@ -1,10 +1,22 @@
 package com.example.app
 
+import com.example.app.service.AuthService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.PostConstruct
 
 @SpringBootApplication
-class AppApplication
+class AppApplication @Autowired constructor( @Qualifier("eagerAuthService") private val authService: AuthService ) {
+    @PostConstruct
+    fun init() {
+        println("Serwis wstrzyknięty do klasy głównej")
+
+        val result = authService.authenticate("admin1", "4321")
+        println("Test autoryzacji admina: $result ")
+    }
+}
 
 fun main(args: Array<String>) {
 	runApplication<AppApplication>(*args)
