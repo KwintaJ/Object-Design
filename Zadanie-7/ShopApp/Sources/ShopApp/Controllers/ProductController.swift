2d41a8e4f0a2bd55527b43303436cebca922509b
Jan Kwinta <jankwinta@icloud.com>
2026-05-27 13:41:38 +0200

    E.7.3.5.27.05 - szablony Leaf

--- Zadanie-7/ShopApp/Sources/ShopApp/Controllers/ProductController.swift
+++ Zadanie-7/ShopApp/Sources/ShopApp/Controllers/ProductController.swift
@@ -4,53 +4,72 @@ import Vapor
 struct ProductController: RouteCollection {
     func boot(routes: any RoutesBuilder) throws {
         let products = routes.grouped("products")
+        
         products.get(use: index)
+        products.get("create", use: createView)
+        products.get(":productID", "edit", use: editView)
+        
         products.post(use: create)
-        products.group(":productID") { product in
-            product.get(use: show)
-            product.put(use: update)
-            product.delete(use: delete)
-        }
+        products.post(":productID", "edit", use: update)
+        products.post(":productID", "delete", use: delete)
     }
 
-    // (GET /products)
-    func index(req: Request) async throws -> [Product] {
-        try await Product.query(on: req.db).all()
+    struct IndexContext: Encodable {
+        let title: String
+        let products: [Product]
     }
 
-    // (POST /products)
-    func create(req: Request) async throws -> Product {
+    struct EditContext: Encodable {
+        let title: String
+        let product: Product
+    }
+
+    // GET /products
+    func index(req: Request) async throws -> View {
+        let products = try await Product.query(on: req.db).all()
+        let context = IndexContext(title: ". s t o r e", products: products)
+        return try await req.view.render("products/index", context)
+    }
+
+    // GET /products/create
+    func createView(req: Request) async throws -> View {
+        return try await req.view.render("products/create", ["title": "Dodaj nowy produkt"])
+    }
+
+    // POST /products
+    func create(req: Request) async throws -> Response {
         let product = try req.content.decode(Product.self)
         try await product.save(on: req.db)
-        return product
+        return req.redirect(to: "/products")
     }
 
-    // (GET /products/:productID)
-    func show(req: Request) async throws -> Product {
+    // GET /products/:productID/edit
+    func editView(req: Request) async throws -> View {
         guard let product = try await Product.find(req.parameters.get("productID"), on: req.db) else {
             throw Abort(.notFound)
         }
-        return product
+        let context = EditContext(title: "Edycja Produktu", product: product)
+        return try await req.view.render("products/edit", context)
     }
 
-    // (PUT /products/:productID)
-    func update(req: Request) async throws -> Product {
-        let updatedProduct = try req.content.decode(Product.self)
+    // POST /products/:productID/edit
+    func update(req: Request) async throws -> Response {
+        let updatedData = try req.content.decode(Product.self)
         guard let product = try await Product.find(req.parameters.get("productID"), on: req.db) else {
             throw Abort(.notFound)
         }
-        product.name = updatedProduct.name
-        product.price = updatedProduct.price
+        product.name = updatedData.name
+        product.price = updatedData.price
         try await product.save(on: req.db)
-        return product
+        return req.redirect(to: "/products")
     }
 
-    // (DELETE /products/:productID)
-    func delete(req: Request) async throws -> HTTPStatus {
+    // POST /products/:productID/delete
+    func delete(req: Request) async throws -> Response {
         guard let product = try await Product.find(req.parameters.get("productID"), on: req.db) else {
             throw Abort(.notFound)
         }
         try await product.delete(on: req.db)
-        return .noContent
+        return req.redirect(to: "/products")
     }
 }
